a2a7ca30096724398a9252956ecabcf30eb9d91b
Checo Chan <xzchan168@gmail.com>
2024-02-22 02:44:50 +0800

    feature: 开发短链接后管系统通用持久层实体对象属性填充

--- admin/src/main/java/com/checo/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/checo/shortlink/admin/dao/entity/UserDO.java
@@ -49,15 +49,18 @@ public class UserDO {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 修改时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     /**
      * 删除标识 0：未删除 1：已删除
      */
+    @TableField(fill = FieldFill.INSERT)
     private int delFlag;
 }
