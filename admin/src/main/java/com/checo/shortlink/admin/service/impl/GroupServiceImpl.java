9140383397479740576d55719030f7f93d7cff64
Checo Chan <xzchan168@gmail.com>
2024-02-22 16:08:28 +0800

    feature: 开发短链接后管系统分组集合

--- admin/src/main/java/com/checo/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/checo/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,13 +1,17 @@
 package com.checo.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.checo.shortlink.admin.dao.entity.GroupDO;
 import com.checo.shortlink.admin.dao.mapper.GroupMapper;
+import com.checo.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.checo.shortlink.admin.service.GroupService;
 import com.checo.shortlink.admin.toolkit.RandomGenerator;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 /**
@@ -24,8 +28,20 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
                 .name(groupName)
+                .sortOrder(0)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        // TODO 获取用户名
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getUsername, "Checo")
+                .eq(GroupDO::getDelFlag, 0)
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
