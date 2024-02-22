d454b4bf80f3719b8662d9a037b5194445b84227
Checo Chan <xzchan168@gmail.com>
2024-02-22 15:41:41 +0800

    feature: 开发短链接后管分组新增功能

--- admin/src/main/java/com/checo/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/checo/shortlink/admin/controller/GroupController.java
@@ -1,7 +1,12 @@
 package com.checo.shortlink.admin.controller;
 
+import com.checo.shortlink.admin.common.convention.result.Result;
+import com.checo.shortlink.admin.common.convention.result.Results;
+import com.checo.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
 import com.checo.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
 
     private final GroupService groupService;
+
+    /**
+     * 新增短链接分组
+     */
+    @PostMapping("/api/short-link/v1/group")
+    public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
+        groupService.saveGroup(requestParam.getName());
+        return Results.success();
+    }
 }
