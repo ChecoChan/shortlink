df326bd6e586ac023e1a5d8c892cb00b1247b0f1
Checo Chan <xzchan168@gmail.com>
2024-02-22 22:29:01 +0800

    feature: 开发短链接后管系统短链接分组修改功能

--- admin/src/main/java/com/checo/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/checo/shortlink/admin/controller/GroupController.java
@@ -3,13 +3,11 @@ package com.checo.shortlink.admin.controller;
 import com.checo.shortlink.admin.common.convention.result.Result;
 import com.checo.shortlink.admin.common.convention.result.Results;
 import com.checo.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.checo.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.checo.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.checo.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +35,14 @@ public class GroupController {
     @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
+    }
+
+    /**
+     * 修改短链接分组名称
+     */
+    @PutMapping("/api/short-link/admin/v1/group")
+    public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam) {
+        groupService.updateGroup(requestParam);
+        return Results.success();
     }
 }
