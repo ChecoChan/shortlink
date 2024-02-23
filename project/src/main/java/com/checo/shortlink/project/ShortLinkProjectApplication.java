2ceb5ad20f0512fe24badf327758710d87572ce8
Checo Chan <xzchan168@gmail.com>
2024-02-23 17:11:29 +0800

    feature: 开发短链接管理新增短链接功能

--- project/src/main/java/com/checo/shortlink/project/ShortLinkProjectApplication.java
+++ project/src/main/java/com/checo/shortlink/project/ShortLinkProjectApplication.java
@@ -1,9 +1,11 @@
 package com.checo.shortlink.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.checo.shortlink.project.dao.mapper")
 public class ShortLinkProjectApplication {
 
     public static void main(String[] args) {
