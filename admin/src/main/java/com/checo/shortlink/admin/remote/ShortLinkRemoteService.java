6821c9cf3cfeb5347b0b1b6d6ef7a2c293c2ea55
Checo Chan <xzchan168@gmail.com>
2024-02-27 01:05:18 +0800

    fix: 去除多余空格

--- admin/src/main/java/com/checo/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/checo/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -89,7 +89,7 @@ public interface ShortLinkRemoteService {
      *
      * @param requestParam 请求参数
      */
-    default void saveRecycleBin( RecycleBinSaveReqDTO requestParam) {
+    default void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
         HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/save", JSON.toJSONString(requestParam));
     }
 
