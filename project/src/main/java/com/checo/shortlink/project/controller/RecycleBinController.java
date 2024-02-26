8a679b6553eab05e49c1d4dd2393694ea4bb1cbf
Checo Chan <xzchan168@gmail.com>
2024-02-27 01:17:19 +0800

    feature: 开发短链接回收站恢复短链接功能

--- project/src/main/java/com/checo/shortlink/project/controller/RecycleBinController.java
+++ project/src/main/java/com/checo/shortlink/project/controller/RecycleBinController.java
@@ -3,6 +3,7 @@ package com.checo.shortlink.project.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.checo.shortlink.project.common.convention.result.Result;
 import com.checo.shortlink.project.common.convention.result.Results;
+import com.checo.shortlink.project.dto.req.RecycleBinRecoverReqDTO;
 import com.checo.shortlink.project.dto.req.RecycleBinSaveReqDTO;
 import com.checo.shortlink.project.dto.req.ShortLinkRecycleBinPageReqDTO;
 import com.checo.shortlink.project.dto.resp.ShortLinkPageRespDTO;
@@ -37,5 +38,14 @@ public class RecycleBinController {
     @GetMapping("/api/short-link/v1/recycle-bin/page")
     public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
         return Results.success(recycleBinService.pageShortLink(requestParam));
+    }
+
+    /**
+     * 恢复短链接
+     */
+    @PostMapping("/api/short-link/v1/recycle-bin/recover")
+    public Result<Void> recoverRecycleBin(@RequestBody RecycleBinRecoverReqDTO requestParam) {
+        recycleBinService.recoverRecycleBin(requestParam);
+        return Results.success();
     }
 }
--- project/src/main/java/com/checo/shortlink/project/service/RecycleBinService.java
+++ project/src/main/java/com/checo/shortlink/project/service/RecycleBinService.java
@@ -3,6 +3,7 @@ package com.checo.shortlink.project.service;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.checo.shortlink.project.dao.entity.ShortLinkDO;
+import com.checo.shortlink.project.dto.req.RecycleBinRecoverReqDTO;
 import com.checo.shortlink.project.dto.req.RecycleBinSaveReqDTO;
 import com.checo.shortlink.project.dto.req.ShortLinkRecycleBinPageReqDTO;
 import com.checo.shortlink.project.dto.resp.ShortLinkPageRespDTO;
@@ -26,4 +27,11 @@ public interface RecycleBinService extends IService<ShortLinkDO> {
      * @return 短链接分页返回结果
      */
     IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam);
+
+    /**
+     * 从回收站恢复短链接
+     *
+     * @param requestParam 恢复短链接请求参数
+     */
+    void recoverRecycleBin(RecycleBinRecoverReqDTO requestParam);
 }
