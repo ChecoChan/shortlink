0131f11ce5cc748060a1dd7a8190a4cc38326465
Checo Chan <xzchan168@gmail.com>
2024-02-27 17:12:12 +0800

    feature: 开发短链接监控之地区访问数据

--- project/src/main/java/com/checo/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/checo/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -6,6 +6,9 @@ import cn.hutool.core.date.Week;
 import cn.hutool.core.text.StrBuilder;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -16,9 +19,11 @@ import com.checo.shortlink.project.common.convention.exception.ClientException;
 import com.checo.shortlink.project.common.convention.exception.ServiceException;
 import com.checo.shortlink.project.common.enums.VailDateTypeEnum;
 import com.checo.shortlink.project.dao.entity.LinkAccessStatsDO;
+import com.checo.shortlink.project.dao.entity.LinkLocaleStatsDO;
 import com.checo.shortlink.project.dao.entity.ShortLinkDO;
 import com.checo.shortlink.project.dao.entity.ShortLinkGotoDO;
 import com.checo.shortlink.project.dao.mapper.LinkAccessStatsMapper;
+import com.checo.shortlink.project.dao.mapper.LinkLocaleStatsMapper;
 import com.checo.shortlink.project.dao.mapper.ShortLinkGotoMapper;
 import com.checo.shortlink.project.dao.mapper.ShortLinkMapper;
 import com.checo.shortlink.project.dto.req.ShortLinkCreateReqDTO;
@@ -44,6 +49,7 @@ import org.jsoup.nodes.Element;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -56,6 +62,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.checo.shortlink.project.common.constant.RedisKeyConstant.*;
+import static com.checo.shortlink.project.common.constant.ShortLinkConstant.APAP_REMOTE_URL;
 
 
 /**
@@ -71,6 +78,10 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     private final StringRedisTemplate stringRedisTemplate;
     private final RedissonClient redissonClient;
     private final LinkAccessStatsMapper linkAccessStatsMapper;
+    private final LinkLocaleStatsMapper linkLocaleStatsMapper;
+
+    @Value("${short-link.stats.locale.amap-key}")
+    private String statsLocaleAmapKey;
 
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
@@ -339,6 +350,27 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .date(new Date())
                     .build();
             linkAccessStatsMapper.shortLinkStats(linkAccessStatsDO);
+            Map<String, Object> localeParamMap = new HashMap<>();
+            localeParamMap.put("key", statsLocaleAmapKey);
+            localeParamMap.put("ip", remoteAddr);
+            String localeResultStr = HttpUtil.get(APAP_REMOTE_URL, localeParamMap);
+            JSONObject localeResultObj = JSON.parseObject(localeResultStr);
+            String infoCode = localeResultObj.getString("infocode");
+            if (StrUtil.isNotBlank(infoCode) && StrUtil.equals(infoCode, "10000")) {
+                String province = localeResultObj.getString("province");
+                boolean unknownFlag = StrUtil.equals("[]", province);
+                LinkLocaleStatsDO linkLocaleStatsDO = LinkLocaleStatsDO.builder()
+                        .province(unknownFlag ? "未知" : province)
+                        .city(unknownFlag ? "未知" : localeResultObj.getString("province"))
+                        .adcode(unknownFlag ? "未知" : localeResultObj.getString("adcode"))
+                        .country("中国")
+                        .fullShortUrl(fullShortUrl)
+                        .cnt(1)
+                        .gid(gid)
+                        .date(new Date())
+                        .build();
+                linkLocaleStatsMapper.shortLinkLocaleStats(linkLocaleStatsDO);
+            }
         } catch (Throwable ex) {
             log.error("短链接访问统计量异常", ex);
         }
