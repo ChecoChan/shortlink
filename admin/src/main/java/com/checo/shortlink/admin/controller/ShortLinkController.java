448ba2d26a37970ce5bdb674591365fb9ce9d78f
Checo Chan <xzchan168@gmail.com>
2024-03-01 15:41:45 +0800

    feature: 开发短链接批量创建功能

--- admin/src/main/java/com/checo/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/checo/shortlink/admin/controller/ShortLinkController.java
@@ -4,15 +4,25 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.checo.shortlink.admin.common.convention.result.Result;
 import com.checo.shortlink.admin.common.convention.result.Results;
 import com.checo.shortlink.admin.remote.ShortLinkRemoteService;
+import com.checo.shortlink.admin.remote.dto.req.ShortLinkBatchCreateReqDTO;
 import com.checo.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.checo.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.checo.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
+import com.checo.shortlink.admin.remote.dto.resp.ShortLinkBaseInfoRespDTO;
+import com.checo.shortlink.admin.remote.dto.resp.ShortLinkBatchCreateRespDTO;
 import com.checo.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.checo.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.checo.shortlink.admin.toolkit.EasyExcelWebUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * 短链接后管控制层
@@ -32,6 +42,21 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 批量创建短链接
+     */
+    @SneakyThrows
+    @PostMapping("/api/short-link/admin/v1/create/batch")
+    public void batchCreateShortLink(@RequestBody ShortLinkBatchCreateReqDTO requestParam, HttpServletResponse response) {
+        Result<ShortLinkBatchCreateRespDTO> shortLinkBatchCreateRespDTOResult = shortLinkRemoteService.batchCreateShortLink(requestParam);
+        if (shortLinkBatchCreateRespDTOResult.isSuccess()) {
+            List<ShortLinkBaseInfoRespDTO> baseLinkInfos = shortLinkBatchCreateRespDTOResult.getData().getBaseLinkInfos();
+            String localDateStr = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            String excelFileName = "批量创建短链接-SaaS短链接系统-" + localDateStr;
+            EasyExcelWebUtil.write(response, excelFileName, ShortLinkBaseInfoRespDTO.class, baseLinkInfos);
+        }
     }
 
     /**
--- project/src/main/java/com/checo/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/checo/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -21,12 +21,11 @@ import com.checo.shortlink.project.common.convention.exception.ServiceException;
 import com.checo.shortlink.project.common.enums.VailDateTypeEnum;
 import com.checo.shortlink.project.dao.entity.*;
 import com.checo.shortlink.project.dao.mapper.*;
+import com.checo.shortlink.project.dto.req.ShortLinkBatchCreateReqDTO;
 import com.checo.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import com.checo.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.checo.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
-import com.checo.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
-import com.checo.shortlink.project.dto.resp.ShortLinkGroupCountQueryRespDTO;
-import com.checo.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import com.checo.shortlink.project.dto.resp.*;
 import com.checo.shortlink.project.service.ShortLinkService;
 import com.checo.shortlink.project.toolkit.HashUtil;
 import com.checo.shortlink.project.toolkit.LinkUtil;
@@ -137,6 +136,33 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .gid(requestParam.getGid())
                 .originUrl(requestParam.getOriginUrl())
                 .fullShortUrl("http://" + shortLinkDO.getFullShortUrl())
+                .build();
+    }
+
+    @Override
+    public ShortLinkBatchCreateRespDTO batchCreateShortLink(ShortLinkBatchCreateReqDTO requestParam) {
+        List<String> originUrls = requestParam.getOriginUrls();
+        List<String> describes = requestParam.getDescribes();
+        List<ShortLinkBaseInfoRespDTO> result = new ArrayList<>();
+        for (int i = 0; i < originUrls.size(); i++) {
+            ShortLinkCreateReqDTO shortLinkCreateReqDTO = BeanUtil.toBean(requestParam, ShortLinkCreateReqDTO.class);
+            shortLinkCreateReqDTO.setOriginUrl(originUrls.get(i));
+            shortLinkCreateReqDTO.setDescribe(describes.get(i));
+            try {
+                ShortLinkCreateRespDTO shortLink = createShortLink(shortLinkCreateReqDTO);
+                ShortLinkBaseInfoRespDTO linkBaseInfoRespDTO = ShortLinkBaseInfoRespDTO.builder()
+                        .fullShortUrl(shortLink.getFullShortUrl())
+                        .originUrl(shortLink.getOriginUrl())
+                        .describe(describes.get(i))
+                        .build();
+                result.add(linkBaseInfoRespDTO);
+            } catch (Throwable ex) {
+                log.error("批量创建短链接失败，原始参数：{}", originUrls.get(i));
+            }
+        }
+        return ShortLinkBatchCreateRespDTO.builder()
+                .total(result.size())
+                .baseLinkInfos(result)
                 .build();
     }
 
