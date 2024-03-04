fb85cd02544b16557da7dfdf9efb201979e79ada
Checo Chan <xzchan168@gmail.com>
2024-03-04 19:45:54 +0800

    fix: 更新工具方法的使用

--- admin/src/main/java/com/checo/shortlink/admin/common/web/GlobalExceptionHandler.java
+++ admin/src/main/java/com/checo/shortlink/admin/common/web/GlobalExceptionHandler.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -73,7 +72,7 @@ public class GlobalExceptionHandler {
     }
 
     private String getUrl(HttpServletRequest request) {
-        if (StringUtils.isEmpty(request.getQueryString())) {
+        if (StrUtil.isBlank(request.getQueryString())) {
             return request.getRequestURL().toString();
         }
         return request.getRequestURL().toString() + "?" + request.getQueryString();
