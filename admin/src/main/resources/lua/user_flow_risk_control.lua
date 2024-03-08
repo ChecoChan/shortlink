ef2ad2f5cbb555106995257aa533b5ff4a6d4933
Checo Chan <xzchan168@gmail.com>
2024-03-08 21:14:25 +0800

    fix: 修复后管创建短链接流量监控每次请求都重置 Redis Key 过期时间问题

--- admin/src/main/resources/lua/user_flow_risk_control.lua
+++ admin/src/main/resources/lua/user_flow_risk_control.lua
@@ -5,11 +5,16 @@ local timeWindow = tonumber(ARGV[1]) -- 时间窗口，单位：秒
 -- 构造 Redis 中存储用户访问次数的键名
 local accessKey = "short-link:user-flow-risk-control:" .. username
 
+-- 使用SETNX命令设置键的值
+local setnxResult = redis.call("SETNX", accessKey, "1")
+
 -- 原子递增访问次数，并获取递增后的值
 local currentAccessCount = redis.call("INCR", accessKey)
 
--- 设置键的过期时间
-redis.call("EXPIRE", accessKey, timeWindow)
+-- 只有设置键成功才给键设置过期时间
+if setnxResult == 1 then
+	redis.call("EXPIRE", accessKey, timeWindow)
+end
 
 -- 返回当前访问次数
 return currentAccessCount
